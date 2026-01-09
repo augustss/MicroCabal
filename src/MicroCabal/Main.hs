e16ad1c30cbb270153b4f16e0d9d25ed82e57f40
Lennart Augustsson <lennart@augustsson.net>
2026-01-09 21:03:52 +0100

    Update ghc-compat version.

--- src/MicroCabal/Main.hs
+++ src/MicroCabal/Main.hs
@@ -169,7 +169,7 @@ distPkgs =
 --  , StackagePackage "deepseq"      (makeVersion [1,6,0,0])  False []  -- built in
   , StackagePackage "exceptions"   (makeVersion [0,10,11])    False []
   , StackagePackage "filepath"     (makeVersion [1,5,4,0])    False []
-  , StackagePackage "ghc-compat"   (makeVersion [0,5,4,0])    False []
+  , StackagePackage "ghc-compat"   (makeVersion [0,5,5,0])    False []
   , StackagePackage "mtl"          (makeVersion [2,3,2])      False []
   , StackagePackage "os-string"    (makeVersion [2,0,7])      False []
   , StackagePackage "parsec"       (makeVersion [3,1,18,0])   False []
